8e5c518f3d151ebf350aab3c079705e34a63ba9b
bobo <jordanbis@orange.fr>
2024-02-14 09:41:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,16 +17,11 @@ def favicon():
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+# Ce code affiche "Hello, world!" lorsque vous exécutez l'application
 
+def main():
+    print("Hello, world!")
 
-if __name__ == '__main__':
-   app.run()
+if __name__ == "__main__":
+    main()
