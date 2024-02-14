92ce0c492da152860d3aed6cc270cc85b3f96e85
bobo <jordanbis@orange.fr>
2024-02-14 15:41:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
 from flask_oidc import OpenIDConnect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(24)
-app.config['OIDC_CLIENT_SECRETS'] = 'client_secrets.json'  # Chemin vers le fichier de configuration client OIDC
+# Configurez ces valeurs avec celles de votre application Azure B2C
+app.config['B2C_TENANT'] = "ab39bc97-fa3e-44e9-a2c6-e0f1aa8183f7"
+app.config['B2C_CLIENT_ID'] = "3c0393e1-d9ea-4d48-84b7-a1f4c3f5c98c"
+app.config['B2C_CLIENT_SECRET'] = "x9E8Q~3c7z12nfit7JCs4w5VI7D7uZmVB_Db_a17"
+app.config['B2C_REDIRECT_URI'] = "https://application-test-bluink.azurewebsites.net"  # Changez pour votre URL de production
+app.config['B2C_AUTHORITY'] = "https://application-test-bluink.azurewebsites.net"
 app.config['OIDC_ID_TOKEN_COOKIE_SECURE'] = False  # Permettre l'utilisation de cookies pour les tokens sur HTTP
 oidc = OpenIDConnect(app)
 
