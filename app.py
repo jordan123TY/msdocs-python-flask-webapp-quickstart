98348617b03e66bc3a1359ebb760d9e10321012a
bobo <jordanbis@orange.fr>
2024-02-14 16:31:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
-from flask_oidc import OpenIDConnect
 
 app = Flask(__name__)
 
